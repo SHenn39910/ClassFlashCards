68d0192a214885bf23d9fd257fd4a19d82c051f4
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-05-12 10:56:56 -0400

    first commit

---
+++ flashcardproject/flashcards.js
@@ -0,0 +1,8 @@
+document.getElementById('btnSave').addEventListener('click', saveCard);
+let Cards = new Array();
+
+function saveCard(){
+    let frontCard = document.getElementById('frontCard').nodeValue;
+    let backCard = document.getElementById('backCard').nodeValue;
+    let card = {front: frontContent, back: backContent}
+}
