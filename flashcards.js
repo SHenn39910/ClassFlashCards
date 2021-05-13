901e13ebfeadc320a0ac03a23e38b78598d12859
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-05-13 15:43:39 -0400

    adjusted the let bindings to match html

--- flashcards.js
+++ flashcards.js
@@ -2,7 +2,20 @@ document.getElementById('btnSave').addEventListener('click', saveCard);
 let Cards = new Array();
 
 function saveCard(){
-    let frontCard = document.getElementById('frontCard').nodeValue;
-    let backCard = document.getElementById('backCard').nodeValue;
-    let card = {front: frontContent, back: backContent}
+    let frontContent = document.getElementById('frontCard').value;
+    let backContent = document.getElementById('backCard').value;
+    let card = {front: frontContent, back: backContent};
+    Cards.push(card);
+    console.log(Cards);
+    eraseCards();
+    numCard();
+}
+
+numCard =()=> {
+    document.getElementById('numCard').innerHTML = Cards.length;
+}
+
+eraseCards = () => {
+    document.getElementById('frontCard').value = "";
+    document.getElementById('backCard'). value= "";
 }
